6fbe87f33fa1263b2e7a190c0cc5367f4e668ddc
Markus Pfeifer <mkspfeifer@gmail.com>
2022-11-30 13:13:13 +0100

    started implementing object localisation

--- object_detection.py
+++ object_detection.py
@@ -6,6 +6,7 @@ from main import Config
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
+from torchvision.ops import generalized_box_iou_loss
 
 
 class BoschDataset(Dataset):
@@ -25,27 +26,26 @@ class BoschDataset(Dataset):
         """Number of images not of labels, so parts of dataset can be used."""
         return len(self.images)
 
-    def __getitem__(self, idx: int) -> (torch.Tensor, list):
+    def __getitem__(self, idx: int) -> (torch.Tensor, torch.Tensor):
         img = Image.open(os.path.join(self.root, self.images[idx]))
         if self.transforms:
             img = self.transforms(img)
-        # If the following code throws a KeyError, then change to second json file.
+        # If the following code throws a KeyError, then change to the second json file.
         aabbs = []  # No list comprehension for better readability.
         x_factor, y_factor = 1232/2464, 1028/2056  # Taking care of smaller image resolution.
         for vehicle in self.labels["./" + self.images[idx].replace('\\', '/')]["vehicles"]:
-            aabbs.append([vehicle["AABB"]["x1"]*x_factor,
-                          vehicle["AABB"]["y1"]*y_factor,
-                          vehicle["AABB"]["x2"]*x_factor,
-                          vehicle["AABB"]["y2"]*y_factor])
-        return img, aabbs
+            # TODO: Change to tensors, so it is easy to calculate with them using autograd.
+            aabbs.append([[vehicle["AABB"]["x1"]*x_factor, vehicle["AABB"]["y1"]*y_factor],
+                          [vehicle["AABB"]["x2"]*x_factor, vehicle["AABB"]["y2"]*y_factor]])
+        return img, torch.tensor(aabbs)
 
     def show(self, idx: int):
-        """Shows the image with the drawn bounding boxes."""
+        """Shows image with the drawn bounding boxes."""
         img, bboxes = self.__getitem__(idx)
         img = transforms.ToPILImage()(img)  # Returns Image.
         painter = ImageDraw.Draw(img)
         for bbox in bboxes:
-            painter.rectangle(bbox, outline=(0, 255, 0))
+            painter.rectangle(bbox.flatten().tolist(), outline=(0, 255, 0))
         img.show()
 
 
@@ -62,4 +62,10 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+
+    prediction = torch.tensor([4., 8., 6., 3.], requires_grad=True)
+    ground_truth = torch.tensor([2., 6., 5., 2.])
+    loss = generalized_box_iou_loss(prediction, ground_truth)
+    loss.backward()
+    print(prediction.grad)
