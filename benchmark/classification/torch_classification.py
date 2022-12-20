d3b502578e6aeada1cd1252c5fd1cd527b39bde1
Markus Pfeifer <mkspfeifer@gmail.com>
2022-12-20 11:13:42 +0100

    changed config.json path

--- benchmark/classification/torch_classification.py
+++ benchmark/classification/torch_classification.py
@@ -129,7 +129,7 @@ def evaluate(model,
 
 def main():
     """Testing the performance of the AlphaModel on the MNIST dataset."""
-    config = Config("config.json")
+    config = Config("../../config.json")
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     train_set = torchvision.datasets.MNIST(
