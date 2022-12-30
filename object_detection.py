b76b782f23a6d596ccada3c75a2427b1fee7af63
Markus Pfeifer <mkspfeifer@gmail.com>
2022-12-30 20:27:19 +0100

    updated readme, implemented torchvision function for drawing bboxes

--- object_detection.py
+++ object_detection.py
@@ -2,15 +2,16 @@
 import os
 from PIL import Image, ImageDraw
 import json
-from main import Config
+from init import Config
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.ops import generalized_box_iou_loss
+from torchvision.utils import draw_bounding_boxes
 
 
 class BoschDataset(Dataset):
-    def __init__(self, transformation=transforms.Compose([transforms.PILToTensor()]), root: str = Config()["boxy"]):
+    def __init__(self, root: str, transformation: transforms.Compose = transforms.Compose([transforms.PILToTensor()])):
         self.root = root
         self.transforms = transformation
         self.images = []  # No list comprehension for better maintainability.
@@ -26,7 +27,12 @@ class BoschDataset(Dataset):
         """Number of images not of labels, so parts of dataset can be used."""
         return len(self.images)
 
-    def __getitem__(self, idx: int) -> (torch.Tensor, torch.Tensor):
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+        """
+        :param idx:
+        :return: image as torch.Tensor, a torch.Tensor holding the coordinates of the bounding boxes
+        (shape: num_bboxes, 4).
+        """
         img = Image.open(os.path.join(self.root, self.images[idx]))
         if self.transforms:
             img = self.transforms(img)
@@ -35,17 +41,16 @@ class BoschDataset(Dataset):
         x_factor, y_factor = 1232/2464, 1028/2056  # Taking care of smaller image resolution.
         for vehicle in self.labels["./" + self.images[idx].replace('\\', '/')]["vehicles"]:
             # TODO: Change to tensors, so it is easy to calculate with them using autograd.
-            aabbs.append([[vehicle["AABB"]["x1"]*x_factor, vehicle["AABB"]["y1"]*y_factor],
-                          [vehicle["AABB"]["x2"]*x_factor, vehicle["AABB"]["y2"]*y_factor]])
+            aabbs.append([vehicle["AABB"]["x1"]*x_factor,
+                          vehicle["AABB"]["y1"]*y_factor,
+                          vehicle["AABB"]["x2"]*x_factor,
+                          vehicle["AABB"]["y2"]*y_factor])
         return img, torch.tensor(aabbs)
 
     def show(self, idx: int):
         """Shows image with the drawn bounding boxes."""
-        img, bboxes = self.__getitem__(idx)
+        img = draw_bounding_boxes(*self.__getitem__(idx))
         img = transforms.ToPILImage()(img)  # Returns Image.
-        painter = ImageDraw.Draw(img)
-        for bbox in bboxes:
-            painter.rectangle(bbox.flatten().tolist(), outline=(0, 255, 0))
         img.show()
 
 
@@ -58,14 +63,19 @@ class VehicleDetector(torch.nn.Module):
 
 
 def main():
-    data = BoschDataset()
+    config = Config("config.json")
+
+    data = BoschDataset(root=config["boxy"])
+
+    data.show(0)
 
 
 if __name__ == "__main__":
-    # main()
+    main()
 
-    prediction = torch.tensor([4., 8., 6., 3.], requires_grad=True)
-    ground_truth = torch.tensor([2., 6., 5., 2.])
-    loss = generalized_box_iou_loss(prediction, ground_truth)
-    loss.backward()
-    print(prediction.grad)
+    # IoU test:
+    # prediction = torch.tensor([4., 8., 6., 3.], requires_grad=True)
+    # ground_truth = torch.tensor([2., 6., 5., 2.])
+    # loss = generalized_box_iou_loss(prediction, ground_truth)
+    # loss.backward()
+    # print(prediction.grad)
