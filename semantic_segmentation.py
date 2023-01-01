876f78a8db0e32806a993182f0ec13eea9641219
Markus Pfeifer <mkspfeifer@gmail.com>
2023-01-01 19:47:25 +0100

    Added a legend for the mask encoding of the VOCCalssification.

--- semantic_segmentation.py
+++ semantic_segmentation.py
@@ -40,6 +40,31 @@ def main():
                                        batch_size=32,
                                        shuffle=True)
 
+    mask_encoding = {
+        0: "background",
+        1: "aeroplane",
+        2: "bicycle",
+        3: "bird",
+        4: "boat",
+        5: "bottle",
+        6: "bus",
+        7: "car",
+        8: "cat",
+        9: "chair",
+        10: "cow",
+        11: "dining table",
+        12: "dog",
+        13: "horse",
+        14: "motorbike",
+        15: "person",
+        16: "potted plant",
+        17: "sheep",
+        18: "sofa",
+        19: "train",
+        20: "tv/monitor",
+        255: "void",
+    }
+
 
 if __name__ == "__main__":
     main()
