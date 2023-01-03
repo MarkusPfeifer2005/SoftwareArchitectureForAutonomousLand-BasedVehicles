cfe4917c974c9abe31e09d44540dda45aac34dd6
Markus Pfeifer <mkspfeifer@gmail.com>
2023-01-03 22:45:43 +0100

    Segmentation masks can now be displayed.

--- semantic_segmentation.py
+++ semantic_segmentation.py
@@ -1,32 +1,62 @@
 #!/usr/bin/env python3.10
-
-from PIL import Image
-
 import torch
 from torch.utils.data import DataLoader
-from torchvision.datasets import VOCSegmentation
 from torchvision import transforms
+from torchvision.datasets import VOCSegmentation
+from torchvision.transforms.functional import to_pil_image, pil_to_tensor
+from torchvision.utils import draw_segmentation_masks
 
+from benchmark.classification.torch_classification import ParentModel, train
 from init import Config
-from benchmark.classification.torch_classification import train, ParentModel
 
 
-class SemanticSegmentor(ParentModel):
-    def __init__(self, image_size: tuple, in_channels: int, out_channels: int):
-        super(SemanticSegmentor, self).__init__()
+class PoolSemanticSegmentator(ParentModel):
+    def __init__(self, image_channels: int, classes: int, name: str = "PoolSemanticSegmentator"):
+        super(PoolSemanticSegmentator, self).__init__(name=name)
         # width, height = image_size
         # kernels = [5, 5, 5, 3, 3, 3, 3, 3, 3, 5, 5, 5]
         # paddings = [kernel//2 for kernel in kernels]
+        # (batch-size,) channels, height, width
+        # 3, 375, 500
+        self.conv1 = torch.nn.Conv2d(in_channels=image_channels, out_channels=10, kernel_size=5, padding=2)
+        self.pool1 = torch.nn.MaxPool2d(kernel_size=2, return_indices=True)
+        self.conv2 = torch.nn.Conv2d(in_channels=10, out_channels=32, kernel_size=3, padding=1)
+        self.conv3 = torch.nn.Conv2d(in_channels=32, out_channels=10, kernel_size=3, padding=1)
+        self.pool2 = torch.nn.MaxUnpool2d(kernel_size=2)
+        self.conv4 = torch.nn.Conv2d(in_channels=10, out_channels=classes, kernel_size=5, padding=3)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv1(x)
+        x, indices = self.pool1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.pool2(x, indices=indices)
+        x = self.conv4(x)
+        x = x[:, :, :375, :500]
+        return x
+
+
+class TransposeConvolutionSemanticSegmentator(ParentModel):
+    def __init__(self, image_channels: int, classes: int, name: str = "TransposeConvolutionSemanticSegmentator"):
+        super(TransposeConvolutionSemanticSegmentator, self).__init__(name=name)
         self.operations = torch.nn.Sequential(
-            # torch.nn.Conv2d(in_channels=in_channels, out_channels=10, kernel_size=5, padding=2, stride=2),
-            # torch.nn.Conv2d(in_channels=10, out_channels=50, kernel_size=3, padding=1, stride=2),
-            # torch.nn.ConvTranspose2d(in_channels=50, out_channels=10, kernel_size=3, padding=1, stride=2),
-            # torch.nn.ConvTranspose2d(in_channels=10, out_channels=out_channels, kernel_size=5, padding=2, stride=2),
-            torch.nn.Conv2d(in_channels, out_channels, kernel_size=5, padding=2, stride=1)
+            torch.nn.Conv2d(in_channels=image_channels, out_channels=5, kernel_size=7, padding=3),
+            torch.nn.Conv2d(in_channels=5, out_channels=5, kernel_size=5, stride=2, padding=2),
+            torch.nn.Conv2d(in_channels=5, out_channels=10, kernel_size=5, stride=1, padding=2),
+            torch.nn.Conv2d(in_channels=10, out_channels=32, kernel_size=5, stride=2, padding=2),
+            torch.nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2),
+            torch.nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1),
+            torch.nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=3, stride=2, padding=1),
+            torch.nn.ConvTranspose2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2),
+            torch.nn.ConvTranspose2d(in_channels=32, out_channels=10, kernel_size=5, stride=2, padding=2),
+            torch.nn.ConvTranspose2d(in_channels=10, out_channels=5, kernel_size=5, stride=1, padding=2),
+            torch.nn.ConvTranspose2d(in_channels=5, out_channels=5, kernel_size=5, stride=2, padding=2),
+            torch.nn.ConvTranspose2d(in_channels=5, out_channels=classes, kernel_size=7, padding=0)  # Without padding.
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.operations(x)
+        x = x[:, :, :375, :500]
         return x
 
 
@@ -43,11 +73,11 @@ def evaluate(model,
         correct += (scores == target).to(torch.int8).sum().item()
         total += torch.numel(target)
 
-    print(f"{correct} of {total} pixels were correct resulting in an accuracy of {correct/total*100:.2f}%.")
-    return correct/total*100
+    print(f"{correct} of {total} pixels were correct resulting in an accuracy of {correct / total * 100:.2f}%.")
+    return correct / total * 100
 
 
-class Mask:
+class MaskedImage:
     # All are sorted. No sets, because sets are not ordered.
     classes = [
         "background",
@@ -99,25 +129,31 @@ class Mask:
     ]
     colors = [tuple(color) for color in colors]
     class_indices = [i for i in range(len(classes))]
-    class_indices[-1] = 255
-    assert len(classes) == len(colors) == len(class_indices),\
+    assert len(classes) == len(colors) == len(class_indices), \
         f"{len(classes) = }, {len(colors) = }, {len(class_indices) = }"
 
-    def __init__(self, image: torch.Tensor):
-        assert isinstance(image, torch.Tensor)
-        image = transforms.ToPILImage()(image)
-        self.image = Image.new("RGB", image.size)
-        self.image.paste(image)
-
-        for width in range(self.image.size[0]):
-            for height in range(self.image.size[1]):
-                masked_value = image.getpixel(xy=(width, height))
-                if masked_value > len(self.classes) - 2:
-                    masked_value = 255
-                self.image.putpixel(xy=(width, height), value=self.colors[self.class_indices.index(masked_value)])
+    def __init__(self, image: torch.Tensor, mask: torch.Tensor):
+        if len(image.shape) == 4:
+            image = torch.squeeze(image, dim=0)  # (channel, height, width)
+        if len(mask.shape) == 2:
+            mask = torch.unsqueeze(mask, dim=0)  # (channel, height, width)
+        attribution_maps = []
+        for class_index in range(len(self.classes)):
+            attribution_maps.append(mask == class_index)
+        masks = torch.cat(attribution_maps, dim=0)
+        self.image = draw_segmentation_masks(image=image, masks=masks)
+        self.image = to_pil_image(self.image)
 
     def show(self):
         self.image.show()
+
+
+def white_to_class_index(mask: torch.Tensor):
+    """Reduce all pixels with the value 255 to the value 21."""
+    twentyone = torch.full_like(input=mask, fill_value=21)
+    mask = torch.where(mask <= 21, mask, twentyone)  # Condition is correct because it must be true so value is kept.
+    assert torch.max(mask).item() <= 21
+    return mask
 
 
 def main():
@@ -130,14 +166,15 @@ def main():
                                  year="2012",
                                  image_set="train",
                                  download=False,
-                                 transform=transforms.Compose([transforms.ToTensor(),
+                                 transform=transforms.Compose([transforms.ToTensor(),  # Reduces range to 0 - 1.
                                                                transforms.Resize(image_size)]),
-                                 target_transform=transforms.Compose([transforms.ToTensor(),
+                                 # https://github.com/pytorch/vision/blob/main/references/segmentation/transforms.py
+                                 target_transform=transforms.Compose([pil_to_tensor,  # Seems to preserve range 0 - 255.
                                                                       transforms.Resize(image_size),
-                                                                      torch.squeeze]))
-    train_loader = DataLoader(dataset=train_data,
-                              batch_size=16,
-                              shuffle=True)
+                                                                      torch.squeeze,  # size = (height, width)
+                                                                      white_to_class_index]))  # classes range 0 - 21
+
+    train_loader = DataLoader(dataset=train_data, batch_size=16, shuffle=True)
     validation_data = VOCSegmentation(root=config["voc-segmentation"],
                                       year="2012",
                                       image_set="val",
@@ -146,23 +183,19 @@ def main():
                                                                     transforms.Resize(image_size)]),
                                       target_transform=transforms.Compose([transforms.ToTensor(),
                                                                            transforms.Resize(image_size),
-                                                                           torch.squeeze]))
-    validation_dataloader = DataLoader(dataset=validation_data,
-                                       batch_size=1,
-                                       shuffle=True)
+                                                                           torch.squeeze,
+                                                                           white_to_class_index]))
+    validation_dataloader = DataLoader(dataset=validation_data, batch_size=1, shuffle=True)
 
-    model = SemanticSegmentor(image_size=image_size, in_channels=3, out_channels=22).to(device)
+    model = PoolSemanticSegmentator(image_channels=3, classes=22).to(device)
+    # model = TransposeConvolutionSemanticSegmentator(image_channels=3, classes=22).to(device)
     optimizer = torch.optim.SGD(params=model.parameters(), lr=0.001)
-
-    evaluate(model=model,
-             dataloader=validation_dataloader,
-             device=device)
 
     train(model=model,
           dataloader=train_loader,
           criterion=torch.nn.CrossEntropyLoss(),
           optimizer=optimizer,
-          epochs=3,
+          epochs=7,
           show_graph=True,
           device=device)
     evaluate(model=model,
@@ -170,13 +203,27 @@ def main():
              device=device)
 
     # Check a sample.
+    data = VOCSegmentation(root=config["voc-segmentation"],
+                           year="2012",
+                           image_set="val",
+                           download=False,
+                           transform=transforms.Compose([pil_to_tensor,  # Seems to preserve range 0 - 255.
+                                                         transforms.Resize(image_size)]),
+                           target_transform=transforms.Compose([pil_to_tensor,
+                                                                transforms.Resize(image_size),
+                                                                torch.squeeze,
+                                                                white_to_class_index]))
     model = model.to("cpu")
     model.eval()
-    image, target = train_data[69]
-    target = Mask(target)
-    prediction = Mask(model(image).argmax(dim=0).to(torch.float32))
+    image, target = data[0]
+    image = image.unsqueeze(dim=0)  # Add a batch dimension.
+    scores = model(image.to(torch.float32))
+    scores = scores.argmax(dim=1)
+
+    ground_truth = MaskedImage(image=image, mask=target)
+    ground_truth.show()
+    prediction = MaskedImage(image=image, mask=scores)
     prediction.show()
-    target.show()
 
 
 if __name__ == "__main__":
